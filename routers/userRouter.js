f5e4780028aa491065311d14ce829bf4dba423f1
kun qian <stkun.qian@gmail.com>
2019-05-19 15:08:27 -0700

    complete basic server

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -9,7 +9,7 @@ router.use(function (req, res, next) {
     next();
 });
 router.route('/')
-    .post(function(req, res){
+    .post(function (req, res) {
         console.log("creating a new user");
         const user = new User();
         user.firstName = req.body.firstName;
@@ -17,11 +17,11 @@ router.route('/')
         user.sex = req.body.sex;
         user.age = req.body.age;
         user.password = req.body.password;
-        user.save(function(error){
-            if(error){
+        user.save(function (error) {
+            if (error) {
                 res.send(error);
             }
-            res.json({message: "User create successfully!"});
+            res.json({ message: "User create successfully!" });
         })
     })
     .get(function (req, res) {
@@ -33,16 +33,45 @@ router.route('/')
             res.json(users);
         });
     });
-    router.route('/:user_id')
-    .delete(function(req, res){
+router.route('/:user_id')
+    .get(function(req, res){
+        console.log(`get this user: ${req.params.user_id}`);
+        User.findById(req.params.user_id, function(error, user){
+            if(error){
+                res.send(error);
+            }
+            res.json(user);
+        })
+    })
+    .put(function(req, res){
+        console.log("modify this user: " + req.params.user_id);
+        User.findById(req.params.user_id, function(error, user){
+            if(error){
+                res.send(error);
+            }
+            ["firstName", "lastName", "sex", "age"].forEach((key)=>{
+                if(req.body[key]){
+                    user[key] = req.body[key];
+                    console.log(`update ${key} value of user`);
+                }
+            });
+            user.save(function(error){
+                if(error){
+                    res.send(error);
+                }
+                res.json({message: `user ${req.params.user_id} has been updated!`});
+            });
+        });
+    })
+    .delete(function (req, res) {
         console.log("delete this user: " + req.params.user_id);
         User.deleteOne({
-            _id:req.params.user_id
-        }, function(error, user){
-            if(error){
+            _id: req.params.user_id
+        }, function (error, user) {
+            if (error) {
                 res.send(error);
             }
             res.json({ message: 'Successfully deleted', deletedUser: user });
         });
-    });
+    })
 module.exports = router;
